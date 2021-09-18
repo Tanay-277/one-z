30dde8820db24102f46d7e3100b31d00737d70e6
Tanay-277 <tanayshinde081@gmail.com>
2021-09-18 11:36:04 +0530

    Home is building

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <div className="home-nav">
+          <div className="greet">Good Evening</div>
+          <div className="time">19:25 pm</div>
+          <div className="day">Monday</div>
+          <div className="date">26th January,2021</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
